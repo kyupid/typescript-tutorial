4734806ae3e61b89f79f8a2285593f44b2401c17
kyupid <iamscvv@gmail.com>
2021-10-18 21:22:33 +0900

    [튜토리얼 #10] Function Signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,3 @@
-// let greet: Function;
-
 // example 1
 let greet: (a: string, b: string) => void;
 
@@ -12,7 +10,7 @@ let calc: (a: number, b: number, c: string) => number;
 
 calc = (numOne: number, numTwo: number, action: string) => {
   if (action === 'add') {
-    return numOne + numTwo;
+    return numOne + numTwo
   } else {
     return numOne - numTwo;
   }
@@ -21,6 +19,8 @@ calc = (numOne: number, numTwo: number, action: string) => {
 // example 3
 let logDetails: (obj: {name: string, age: number}) => void;
 
-logDetails = (ninja: {name: string, age: number}) => {
+type person = {name: string, age: number};
+
+logDetails = (ninja: person) => {
   console.log(`${ninja.name} is ${ninja.age} years old`);
 }
