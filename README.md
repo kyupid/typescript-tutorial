8cef9b43006989cdeef7b1b0fcb497f8f5c0c13a
kyupid <iamscvv@gmail.com>
2021-10-19 23:38:51 +0900

    [튜토리얼 #15] Interfaces

---
+++ README.md
@@ -0,0 +1,11 @@
+### Interface
+
+Interface allows you to enforce a certain structrue of a class or an object.
+
+You can use it to describe what properties and what methods and what the types of those properties and return types of those methods are.
+
+You might be thinking it sounds very much like a class but It's different from a class.
+
+You don't use an interface to generate objects or create new objects based on the interface.
+
+You just use it to enforce a certain type of structure within classes or objects. 
--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,27 @@
-import { Invoice } from './classes/Invoice.js';
-
-// interfaces
-export interface IsPerson {
+interface IsPerson {
   name: string;
-  age?: number;
+  age: number;
   speak(a: string): void;
   spend(a: number): number;
 }
 
 const me: IsPerson = {
-  name: 'shaun',
-  //age: 30,
+  name: 'kyu',
+  age: 13,
   speak(text: string): void {
     console.log(text);
   },
   spend(amount: number): number {
-    console.log('I spent ', amount);
+    console.log('I spent', amount);
     return amount;
-  },
+  }
+
 };
 
-console.log(me);
-me.speak('hello, world');
-
-const greetPerson = (person: IsPerson): void => {
+const greetPerson = (person: IsPerson) => {
   console.log('hello ', person.name);
 }
 
 greetPerson(me);
-//greetPerson({name: 'shaun'});
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
-
-// inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
-
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
-
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
-});
+console.log(me);
